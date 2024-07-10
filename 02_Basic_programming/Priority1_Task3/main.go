96558a6ada11b19f2cbb20b0009c83f2c261713c
Nadia-321 <Nadiazimi321@gmail.com>
2024-07-10 18:46:22 +0700

    Update main.go

--- 02_Basic_programming/Priority1_Task3/main.go
+++ 02_Basic_programming/Priority1_Task3/main.go
@@ -1,9 +1,19 @@
+
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("")
+	for i := 1; i <= 100; i++ {
+		switch {
+		case i%4 == 0 && i%7 == 0:
+			fmt.Println("buzz")
+		case i%4 == 0:
+			fmt.Println(i * i)
+		case i%7 == 0:
+			fmt.Println(i * i * i)
+		default:
+			fmt.Println(i)
+		}
+	}
 }
